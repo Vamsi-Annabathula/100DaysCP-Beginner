63d0184a25e3df3ed703ca327213de2f4239050d
Horlicks <vamsi.annabathula@outlook.com>
2021-06-07 00:16:01 +0530

    folder structure update

--- 04-06-2021-watermelon.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int weightOfWatermelon;
-	cin >> weightOfWatermelon;
-	puts(weightOfWatermelon > 3 and weightOfWatermelon %2 == 0 ? "YES" : "NO");
-	return 0;
-}
