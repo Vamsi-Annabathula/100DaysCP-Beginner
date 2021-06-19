ddb469ce9980407ce1cb1613a54d5e7e98b85365
Horlicks Boy <42909554+Vamsi-Annabathula@users.noreply.github.com>
2021-06-19 23:33:37 +0530

    Update sashaTrip.js

--- 1113A-sashaTrip/sashaTrip.js
+++ 1113A-sashaTrip/sashaTrip.js
@@ -1,6 +1,10 @@
-const inp = readline().split(' ')
-for (var i = 0; i < inp.length; i++) {
-    inp[i] = parseInt(inp[i], 10)
+const inp = readline().split(' ').map((e) => { return parseInt(e, 10) })
+var res = inp[1]
+if (inp[1] >= inp[0]) {
+    print(inp[0] - 1)
+} else {
+    for (var i = 2; i <= inp[0] - inp[1]; i++) {
+        res = res + i
+    }
+    print(res)
 }
-var res = Math.floor(inp[0] / inp[1]) * inp[1]
-print(res)
