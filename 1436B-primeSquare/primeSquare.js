f8d9df4c00208e52466abcf0e1ead5cb1a1924a9
Horlicks Boy <42909554+Vamsi-Annabathula@users.noreply.github.com>
2021-06-26 22:55:46 +0530

    primeSquare from codeforces

---
+++ 1436B-primeSquare/primeSquare.js
@@ -0,0 +1,16 @@
+const readline = require("readline-sync")
+const cases = readline.question();
+const isPrime = (n) => {
+  if (n <= 1)
+    return false;
+  for (let i = 2; i < n / 2; i++)
+    if (n % i == 0)
+      return false;
+  return true;
+}
+for (var i = 0; i < cases; i++) {
+  const inp = readline.question()
+  for (var j = 0; j < inp; j++) {
+      
+  }
+}
